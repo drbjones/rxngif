9d9ea204a0a2029c6c56b4730ef1c0dc374077eb
drbjones <b-jones@kellogg.northwestern.edu>
2014-05-15 16:44:32 -0500

    R and start of C
    finished read
    stuck on create

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,18 @@
 class PicturesController < ApplicationController
+
   def show
+    @picture_details = Picture.find_by({ :id => params[:id] })
   end
+
+  def index
+    @list_of_pictures = Picture.all
+  end
+
+  def new
+    # p = Picture.new
+    # p.source = params[:source]
+    # p.caption = params[:caption]
+    # p.save
+  end
+
 end
